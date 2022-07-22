ab2b37bf76a5b739ef0361125530737a3b55b423
Oshaba Samson <chuksdsilent@gmail.com>
2022-07-22 09:24:42 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import styled from 'styled-components'
+function App() {
+
+
+  const Container = styled.div``
+  return (
+    <Container>
+      <BrowserRouter>
+          <Routes></Routes>
+          </BrowserRouter>
+        </Container>
+  );
+}
+
+export default App;
---
+++ src/utils/Theme.js
@@ -0,0 +1,14 @@
+export const darkTheme = {
+    bg: "#181818",
+    bgLight: "#202020",
+    text: "white",
+    textSoft: "#aaaaaa",
+    soft: "#373737"
+}
+export const lightTheme = {
+    bg: "#f9f9f9",
+    bgLight: "white",
+    text: "black",
+    textSoft: "#606060",
+    soft: "#F5F5F5"
+}
